1c65b31f3c396dd9cd52788f2bf6ad5be5b4c851
dubois <dubois.iutlens@gmail.com>
2021-05-10 18:51:54 +0200

    Ajout d'informations

--- report.sh
+++ report.sh
@@ -2,16 +2,25 @@
 #### Config
 depot="carte"
 resultat="diagnostique.txt"
+if [ ! -f ./report.sh ]; then
+  echo "Ce script foit être lancé dans le répertoire du projet"
+  exit 1
+fi
+echo "Ce script va générer le fichier ($resultat), à déposer sur moodle."
+echo "Merci de fournir les informations suivantes :"
+echo
+read -p "Nom et prénom :" nom
+read -p "Groupe (ex A1) :" groupe
 read -p "Votre login sur GitHub :" login
-read -p "Etiez-vous intégrateur ? (o/N) " inte
+read -p "Etiez-vous intégrateur sur cette séance ? (o/N) " inte
 case "$inte" in
-  y|Y|o|O ) inte="O"; etreinte="être intégrateur";;
-  * ) inte="N"; etreinte="ne pas être intégrateur";;
+  y|Y|o|O ) inte="O"; etreinte="intégrateur";;
+  * ) inte="N"; etreinte="pas intégrateur";;
 esac
 ### Analyse
 remote="$(git remote -v)"
 github="https://github.com/$login/$depot"
-echo "`whoami` $github">$resultat
+echo "$nom $groupe (`whoami`) $github">$resultat
 if [[ "$remote" =~ origin[[:space:]]https://github.com/([^/]*)/$depot ]]; then
   origin="${BASH_REMATCH[1]}"
 fi
@@ -31,15 +40,18 @@ echo
 echo "Vérifications"
 echo "============="
 echo
-echo "1) Lien vers votre dépôt sur github : $github (cliquez sur le lien ou copiez/collez dans un navigateur pour vérifier)"
-echo "2) Vous avez indiqué $etreinte"
-echo "3) Un fichier $resultat résultat a normalement été créé dans le répertoire courant. Vérifiez qu'il existe et qu'il contient le lien vers votre dépôt github en première ligne"
+echo "Vous êtes $nom ($groupe), $etreinte"
+echo "Votre dépôt sur GitHub : $github"
+echo
+echo "1) Vérifiez les informations"
+echo "2) Ouvrez le lien dans un navigateur pour vérifier qu'il est correct"
+echo "3) Un fichier $resultat a normalement été créé dans le répertoire courant. Vérifiez qu'il existe et qu'il contient le lien vers votre dépôt github en première ligne"
 echo
 echo "Si une des vérifications ne donne pas le résultat attendu, relancez ce script"
 echo
 echo "Rendu"
 echo "====="
 echo
-echo "Après les vérifications, déposez le fichier $resultat sur le devoir moodle (attention, c'est bien le fichier $resultat qu'il faut déposer, pas report.sh)"
+echo "Après les vérifications, déposez le fichier $resultat sur le devoir moodle (attention, c'est bien le fichier $resultat qu'il faut déposer, pas report.sh. Ne pas renommer le fichier)"
 echo
 read -p "Appuyez sur Entrée pour quitter" entree
